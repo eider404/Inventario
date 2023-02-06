8e0114a703244534ea57e131485b13a7a5a29e73
Eider Pool <eider.pool.arjona@gmail.com>
2023-02-06 11:34:53 -0600

    comentarios agragados

--- src/routes.js
+++ src/routes.js
@@ -7,28 +7,29 @@ require('dotenv').config();
 
 routes.post('/register', async(req, res)=>{
     const user = req.body;
-
+    //filtro para saber si ingresaron los campos
     if(!(user.email && user.username && user.password)){
         return res.status(401).json({status: 401, mensaje: "Username y password son nesesarios"})
     }
 
     req.getConnection((err, conn)=>{
         if(err) { return res.send(err)}
-        
+        //filtro para saber si el email ya a sido usado
         conn.query("SELECT * FROM User WHERE email = ?", [user.email],async (err, rows)=>{
             try {
+                //si rows.length es mayor a 0 el email esta usado, y lo envia al catch
                 if(rows.length > 0){ throw new Error()}
-                
+                //encripta la contrasena
                 const salt = await bcrypt.genSalt(5); 
                 user.password = await bcrypt.hash(user.password, salt);
-
+                //genera un id al usuario
                 user.id = crypto.randomBytes(5).toString("hex");
-
+                //genera el JWT
                 const token = GenerarToken(user.id)
 
                 req.getConnection((err, conn)=>{
                     if(err) { return res.send(err)}
-
+                    //inserta el usuario a la db
                     conn.query("INSERT INTO User set ?", [user], (err, rows)=>{
                         if(err) { return res.send(err) }
                         return res.status(200).json({status: 200, mensaje: "Usuario Registrado", token: token})
@@ -39,9 +40,7 @@ routes.post('/register', async(req, res)=>{
                 return res.status(401).json({status: 401, mensaje: "El email ya esta registrado"})
             }
         })  
-    })
-    
-    
+    }) 
 })
 
 function GenerarToken(userId){
