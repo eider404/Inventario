803adf9a0dafd2ce9334967e5280add6219d1dda
Eider Pool <eider.pool.arjona@gmail.com>
2023-02-06 01:46:16 -0600

    endpoint 'register' filtro de email existente

--- src/routes.js
+++ src/routes.js
@@ -11,23 +11,37 @@ routes.post('/register', async(req, res)=>{
     if(!(user.email && user.username && user.password)){
         return res.status(401).json({status: 401, mensaje: "Username y password son nesesarios"})
     }
-    
-    const salt = await bcrypt.genSalt(5); 
-    user.password = await bcrypt.hash(user.password, salt);
-
-    user.id = crypto.randomBytes(5).toString("hex");
-
-    const token = GenerarToken(user.id)
 
     req.getConnection((err, conn)=>{
         if(err) { return res.send(err)}
+        
+        conn.query("SELECT * FROM User WHERE email = ?", [user.email],async (err, rows)=>{
+            try {
+                if(rows.length > 0){ throw new Error()}
+                
+                const salt = await bcrypt.genSalt(5); 
+                user.password = await bcrypt.hash(user.password, salt);
 
-        conn.query("INSERT INTO User set ?", [user], (err, rows)=>{
-            if(err) { return res.send(err) }
-            return res.status(200).json({status: 200, mensaje: "Usuario Registrado", token: token})
-            
+                user.id = crypto.randomBytes(5).toString("hex");
+
+                const token = GenerarToken(user.id)
+
+                req.getConnection((err, conn)=>{
+                    if(err) { return res.send(err)}
+
+                    conn.query("INSERT INTO User set ?", [user], (err, rows)=>{
+                        if(err) { return res.send(err) }
+                        return res.status(200).json({status: 200, mensaje: "Usuario Registrado", token: token})
+                        
+                    })  
+                })
+            } catch (error) {
+                return res.status(401).json({status: 401, mensaje: "El email ya esta registrado"})
+            }
         })  
     })
+    
+    
 })
 
 function GenerarToken(userId){
