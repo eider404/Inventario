a2c9dd0e5cddad7484d22bcee4132191a1f2a909
Eider Pool <eider.pool.arjona@gmail.com>
2023-02-06 01:27:33 -0600

    JWT agregado en 'register'

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,8 @@ const {Router} = require("express")
 const routes = Router()
 const bcrypt = require('bcrypt');
 const crypto = require("crypto");
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 routes.post('/register', async(req, res)=>{
     const user = req.body;
@@ -15,15 +17,21 @@ routes.post('/register', async(req, res)=>{
 
     user.id = crypto.randomBytes(5).toString("hex");
 
+    const token = GenerarToken(user.id)
+
     req.getConnection((err, conn)=>{
         if(err) { return res.send(err)}
 
         conn.query("INSERT INTO User set ?", [user], (err, rows)=>{
             if(err) { return res.send(err) }
-            return res.status(200).json({status: 200, mensaje: "Usuario Registrado"})
+            return res.status(200).json({status: 200, mensaje: "Usuario Registrado", token: token})
             
         })  
     })
 })
+
+function GenerarToken(userId){
+    return jwt.sign( {userId}, process.env.SECRET, { expiresIn: 60 * 60 * 24});
+}
 
 module.exports = routes
