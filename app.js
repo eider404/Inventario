69d54f43969acc2d27b9f0c9419b2cfeeecf956d
Eider Pool <eider.pool.arjona@gmail.com>
2023-02-05 22:24:47 -0600

    coneccion a la db

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express()
 
+const db = require('./src/models/db')
 
-
+try {
+    app.use(db.myconnection(db.mysql, db.dbOptions,'single'))
+    console.log(`Coneccion a ${db.dbOptions.database} exitosa `)
+} catch (error) {
+    console.log(error)
+}
 
 app.listen(3000,()=>{
     console.log('SERVER RUNING ON PORT 3000...')
