afa8771863033f0f3965048011669ce328d05bda
Eider Pool <eider.pool.arjona@gmail.com>
2023-02-07 12:07:33 -0600

    formulario 'Registrar' conectado al backend

--- public/index.js
+++ public/index.js
@@ -25,3 +25,44 @@ function generarTabla(data){
         `
     } 
 }
+
+
+
+function Register(){
+    let registerForm = document.querySelector( '#registerForm' )
+
+    const obj = {}
+    new FormData( registerForm ).forEach( ( value, key ) => obj[ key ] = value )
+    fetch( `http://localhost:3000/register`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify( obj )
+            })
+            .then(res => res.json())
+            .then( data =>{
+                if(data.status == 401){
+                    return document.querySelector("#message").innerHTML= `
+                        <br>
+                        <div class="alert alert-danger" role="alert">
+                        ${data.mensaje}
+                        </div>
+                    `;
+                }
+
+                document.querySelector("#message").innerHTML= `
+                <br>
+                <div class="alert alert-primary" role="alert">
+                  ${data.mensaje}
+                </div>
+                `; 
+                
+                localStorage.setItem('token', data.token)
+                
+            })
+            .catch(err => console.log(err));
+}
+
+
+/* Register() */
