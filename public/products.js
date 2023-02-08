c79c363a2bfda3cc992dacf95aa286869fc0902f
Eider Pool <eider.pool.arjona@gmail.com>
2023-02-07 20:11:28 -0600

    form 'Actualizar' conectado al backend

--- public/products.js
+++ public/products.js
@@ -33,3 +33,40 @@ function Agregar(){
             })
             .catch(err => console.log(err));
 }
+
+
+function Actualizar(){
+    let actualizarForm = document.querySelector( '#actualizarForm' )
+
+    const obj = {}
+    new FormData( actualizarForm ).forEach( ( value, key ) => obj[ key ] = value )
+
+    fetch( `http://localhost:3000/product/${obj.idProduct}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': localStorage.getItem('token')
+                },
+                body: JSON.stringify( obj )
+            })
+            .then(res => res.json())
+            .then( data =>{
+                if(data.status == 401){
+                    return document.querySelector("#messageAgregar").innerHTML= `
+                        <br>
+                        <div class="alert alert-danger" role="alert">
+                        ${data.mensaje}
+                        </div>
+                    `;
+                }
+
+                document.querySelector("#messageAgregar").innerHTML= `
+                <br>
+                <div class="alert alert-primary" role="alert">
+                  ${data.mensaje}
+                </div>
+                `;                
+                
+            })
+            .catch(err => console.log(err));
+}
